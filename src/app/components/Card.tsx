14201be4255139523cd6c5327ce87d4f3461f364
Apoorv Taneja <purutaneja.com@gmail.com>
2023-09-21 12:47:11 +0530

    fix css for light mode

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -7,7 +7,7 @@ type CardProps = {
 }
 const Card = ({ heading = '', subHeading = '', Icon }: CardProps) => {
   return (
-    <div className="max-w-sm  w-full lg:w-4/12 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-stone-950 dark:border-gray-700">
+    <div className="max-w-sm  w-full lg:w-4/12 p-6 border rounded-lg shadow bg-stone-950 border-gray-700">
       <div className="flex justify-center">
         <Icon className="h-10 w-10" />
       </div>
